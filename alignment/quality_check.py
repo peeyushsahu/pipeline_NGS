8d61273ec6708eef495b0633bfcefeed6dae1c91
peeyushsahu <peeyushsahu@yahoo.com>
2017-07-14 14:35:01 +0200

    Multiprocessing fixed for fast_qc

--- alignment/quality_check.py
+++ alignment/quality_check.py
@@ -10,14 +10,15 @@ import collections
 from Bio.SeqUtils import GC
 
 path = '/ps/imt/Pipeline_development/raw_data/chipseq/singleEnd/40_K4me3_WT10_ChIP25_110915_TGACCA_L001_R1_003.fastq'
-#zipped_path = '/ps/imt/Pipeline_development/raw_data/chipseq/singleEnd/40_K4me3_WT10_ChIP25_110915_TGACCA_L001_R1_003.fastq.gz'
-zipped_path = '/home/peeyush/PycharmProjects/pipeline_development/part_test_file.fastq.gz'
+zipped_path = '/ps/imt/Pipeline_development/raw_data/chipseq/singleEnd/40_K4me3_WT10_ChIP25_110915_TGACCA_L001_R1_003.fastq.gz'
+# zipped_path = '/home/peeyush/PycharmProjects/pipeline_development/part_test_file.fastq.gz'
 
 base_freq_dict = {key: dict.fromkeys(['A', 'T', 'G', 'C', 'N'], 0) for key in range(51)}
 nucleotide_phred_quality_dict = {key: dict.fromkeys([i for i in range(1, 43, 1)], 0) for key in range(51)}
 seq_length_dist = dict.fromkeys(range(1, 101, 1), 0)
 seq_gc_dict = dict.fromkeys(range(0, 101, 1), 0)
 seq_qual_dict = dict.fromkeys(range(1, 43, 1), 0)
+flowcell_tile = set()
 
 
 def fastq_iterator(file_obj):
@@ -38,11 +39,17 @@ def seq_qual_check(qual_str):
     """calculates per base and average seq quality"""
     avg_qual = 0
     for ind, asci in enumerate(qual_str):
-        qual = ord(asci)-33
+        qual = ord(asci) - 33
         nucleotide_phred_quality_dict[ind][qual] += 1
         avg_qual += qual
     avg_qual /= len(qual_str)
     seq_qual_dict[round(avg_qual)] += 1
+
+
+def seq_id_ops(id):
+    """Check for flowcell"""
+    id_elements = id.split(':')
+    flowcell_tile.add(id_elements[4])
 
 
 def nucleotide_operations(seq):
@@ -59,19 +66,18 @@ def nucleotide_operations(seq):
 
     # calculating % GC in seq
     letters = collections.Counter(seq)
-    GC = round(((letters['G']+letters['C'])/51)*100)
+    GC = round(((letters['G'] + letters['C']) / 51) * 100)
     seq_gc_dict[GC] += 1
 
 
 start = timeit.default_timer()
 
 i = 0
-#with io.TextIOWrapper(gzip.open(zipped_path, "r"), newline='\n') as handle:
+# with io.TextIOWrapper(gzip.open(zipped_path, "r"), newline='\n') as handle:
 with gzip.open(zipped_path, "rb") as handle:
-
     try:
         for id, seq, phred_qual in fastq_iterator(handle):
-
+            id = id.decode('utf-8').strip('\n')
             seq = seq.decode('utf-8').strip('\n')
             phred_qual = phred_qual.decode('utf-8').strip('\n')
             if not len(seq) == len(phred_qual):
@@ -80,150 +86,17 @@ with gzip.open(zipped_path, "rb") as handle:
 
             nucleotide_operations(seq)
             seq_qual_check(phred_qual)
+            seq_id_ops(id)
             i += 1
+            # if i == 50000:
+            #    break
     except Exception as e:
         print(e)
-        raise ValueError('number of seq:'+str(i))
+        raise ValueError('number of seq:' + str(i))
     finally:
         handle.close()
 
 stop = timeit.default_timer()
-
-
-def fast_qc(zipped_file_path):
-    print('I am in')
-    base_freq_dict = {key: dict.fromkeys(['A', 'T', 'G', 'C', 'N'], 0) for key in range(51)}
-    nucleotide_phred_quality_dict = {key: dict.fromkeys([i for i in range(1, 43, 1)], 0) for key in range(51)}
-    seq_length_dist = dict.fromkeys(range(1, 101, 1), 0)
-    seq_gc_dict = dict.fromkeys(range(0, 101, 1), 0)
-    seq_qual_dict = dict.fromkeys(range(1, 43, 1), 0)
-
-    def fastq_iterator(file_obj):
-        """ A very simple generater for reading fastq files"""
-        id = file_obj.readline()
-        seq = file_obj.readline()
-        info = file_obj.readline()
-        phred_qual = file_obj.readline()
-        while id:
-            yield (id, seq, phred_qual)
-            id = file_obj.readline()
-            seq = file_obj.readline()
-            info = file_obj.readline()
-            phred_qual = file_obj.readline()
-
-    def seq_qual_check(qual_str):
-        """calculates per base and average seq quality"""
-        avg_qual = 0
-        for ind, asci in enumerate(qual_str):
-            qual = ord(asci)-33
-            nucleotide_phred_quality_dict[ind][qual] += 1
-            avg_qual += qual
-        avg_qual /= len(qual_str)
-        seq_qual_dict[round(avg_qual)] += 1
-
-    def nucleotide_operations(seq):
-        """calculates frequency of nucleotide at every position"""
-        # calculating base freq
-        max_length = 1000
-        seq_len = len(seq)
-        if seq_len > max_length:
-            raise ('Not a valid fastq file OR sequence ')
-
-        seq_length_dist[len(seq)] += 1
-        for ind, base in enumerate(seq):
-            base_freq_dict[ind][base] += 1
-
-        # calculating % GC in seq
-        letters = collections.Counter(seq)
-        GC = round(((letters['G']+letters['C'])/51)*100)
-        seq_gc_dict[GC] += 1
-
-    with gzip.open(zipped_file_path, "rb") as handle:
-        i = 0
-        try:
-            for id, seq, phred_qual in fastq_iterator(handle):
-
-                seq = seq.decode('utf-8').strip('\n')
-                phred_qual = phred_qual.decode('utf-8').strip('\n')
-                if not len(seq) == len(phred_qual):
-                    print(seq, '\n', phred_qual)
-                    raise ValueError('Broken fastq file...')
-
-                nucleotide_operations(seq)
-                seq_qual_check(phred_qual)
-                i += 1
-        except Exception as e:
-            print(e)
-            raise ValueError('number of seq analysed:'+str(i))
-        finally:
-            handle.close()
-
-    print({'seq_qual_dict': seq_qual_dict,
-    'nucleotide_phred_quality_dict': nucleotide_phred_quality_dict,
-    'base_freq_dict': base_freq_dict,
-    'seq_length_dist': seq_length_dist,
-    'seq_gc_dict': seq_gc_dict})
-    return {'seq_qual_dict': seq_qual_dict,
-    'nucleotide_phred_quality_dict': nucleotide_phred_quality_dict,
-    'base_freq_dict': base_freq_dict,
-    'seq_length_dist': seq_length_dist,
-    'seq_gc_dict': seq_gc_dict}
-
-
-def mp_fastqc(fq_filepaths):
-    import multiprocessing
-
-    def worker(args, input_queq, reqult_queq):
-
-        while True:
-        #for fq_path in iter(input_queq.get()):
-            fq_path = input_queq.get()
-            print(fq_path)
-            if fq_path is None:
-                print('Worker % is exiting' %args)
-                break
-            sample_name = fq_path.split('/')[-1]
-            out_dict = {}
-            try:
-                out_dict[sample_name] = fast_qc(fq_path)
-                reqult_queq.put(out_dict)
-            except:
-                reqult_queq.put('Error:'+fq_path)
-        return
-
-    input_queq = multiprocessing.Queue()
-    out_queq = multiprocessing.Queue()
-    nproc = []
-
-    no_cpus = int(multiprocessing.cpu_count())
-    print('Creating %d of consumers' %no_cpus)
-
-    for i in range(no_cpus):
-        p = multiprocessing.Process(
-            target=worker,
-            args=(i, input_queq, out_queq))
-        nproc.append(p)
-        p.start()
-
-    for file in fq_filepaths:
-        input_queq.put(file)
-
-    for i in range(no_cpus):
-        input_queq.put(None)
-
-    result_dict = {}
-    result = out_queq.qsize()
-    while result:
-        result_dict.update(out_queq.get())
-        result -= 1
-
-    for p in nproc:
-        p.join()
-
-    print(len(result_dict))
-    print(result_dict)
-
-
 
 '''
 ## Bio.SeqIO parser for fastq (this thing is atleast 3X slowwer then self written parser)
@@ -253,13 +126,14 @@ with io.TextIOWrapper(gzip.open(zipped_path, "r")) as handle:
 '''
 
 print('Total seq analysed:', i)
-print('Time spent in creating the quality control:', (stop-start))
+print('Time spent in creating the quality control:', (stop - start))
 
 print(seq_qual_dict)
 print(nucleotide_phred_quality_dict)
 print(base_freq_dict)
 print(seq_length_dist)
 print(seq_gc_dict)
+print(flowcell_tile)
 
 '''
 pd.DataFrame(nucleotide_phred_quality_dict, index=range(42, 0, -1)).to_csv('/ps/imt/Pipeline_development/results/AlignedLane/phred_quality_matrix.tsv', sep='\t')
@@ -282,3 +156,151 @@ with open('/ps/imt/Pipeline_development/results/AlignedLane/seq_quality_distribu
         file.write(str(key)+'\t'+str(seq_qual_dict[key])+'\n')
 file.close()
 '''
+
+
+def fast_qc(zipped_file_path):
+    base_freq_dict = {key: dict.fromkeys(['A', 'T', 'G', 'C', 'N'], 0) for key in range(51)}
+    nucleotide_phred_quality_dict = {key: dict.fromkeys([i for i in range(1, 43, 1)], 0) for key in range(51)}
+    seq_length_dist = dict.fromkeys(range(1, 101, 1), 0)
+    seq_gc_dict = dict.fromkeys(range(0, 101, 1), 0)
+    seq_qual_dict = dict.fromkeys(range(1, 43, 1), 0)
+    flowcell_tile = set()
+
+    def fastq_iterator(file_obj):
+        """ A very simple generater for reading fastq files"""
+        id = file_obj.readline()
+        seq = file_obj.readline()
+        info = file_obj.readline()
+        phred_qual = file_obj.readline()
+        while id:
+            yield (id, seq, phred_qual)
+            id = file_obj.readline()
+            seq = file_obj.readline()
+            info = file_obj.readline()
+            phred_qual = file_obj.readline()
+
+    def seq_qual_check(qual_str):
+        """calculates per base and average seq quality"""
+        avg_qual = 0
+        for ind, asci in enumerate(qual_str):
+            qual = ord(asci) - 33
+            nucleotide_phred_quality_dict[ind][qual] += 1
+            avg_qual += qual
+        avg_qual /= len(qual_str)
+        seq_qual_dict[round(avg_qual)] += 1
+
+    def seq_id_ops(id):
+        """Check for flowcell"""
+        id_elements = id.split(':')
+        flowcell_tile.add(id_elements[4])
+
+    def nucleotide_operations(seq):
+        """calculates frequency of nucleotide at every position"""
+        # calculating base freq
+        max_length = 1000
+        seq_len = len(seq)
+        if seq_len > max_length:
+            raise ValueError('Not a valid fastq file OR sequence seq length > max_length')
+
+        seq_length_dist[len(seq)] += 1
+        for ind, base in enumerate(seq):
+            base_freq_dict[ind][base] += 1
+        # calculating % GC in seq
+        letters = collections.Counter(seq)
+        GC = round(((letters['G'] + letters['C']) / 51) * 100)
+        seq_gc_dict[GC] += 1
+
+    with gzip.open(zipped_file_path, "rb") as handle:
+        i = 0
+        try:
+            for id, seq, phred_qual in fastq_iterator(handle):
+                id = id.decode('utf-8').strip('\n')
+                seq = seq.decode('utf-8').strip('\n')
+                phred_qual = phred_qual.decode('utf-8').strip('\n')
+                if not len(seq) == len(phred_qual):
+                    print('Error in:', zipped_file_path)
+                    print(id)
+                    print(seq)
+                    print(phred_qual)
+                    raise ValueError('Broken fastq file...')
+
+                nucleotide_operations(seq)
+                seq_qual_check(phred_qual)
+                seq_id_ops(id)
+                i += 1
+        except Exception as e:
+            print('number of seq analysed:' + str(i))
+            raise ValueError(e)
+        finally:
+            handle.close()
+    return {'seq_qual_dict': seq_qual_dict,
+            'nucleotide_phred_quality_dict': nucleotide_phred_quality_dict,
+            'base_freq_dict': base_freq_dict,
+            'seq_length_dist': seq_length_dist,
+            'seq_gc_dict': seq_gc_dict,
+            'flowcell_tile': flowcell_tile
+            }
+
+
+def mp_fastqc(fq_filepaths):
+    import multiprocessing
+
+    def worker(args, job_queq, reqult_queq):
+
+        while True:
+            fq_path = job_queq.get()
+            print(fq_path)
+            if fq_path is None:
+                print('Worker-%d is exiting' % args)
+                break
+            sample_name = fq_path.split('/')[-1]
+            out_dict = {}
+            print('Worker-%d started the job' % args)
+            try:
+                out_dict[sample_name] = fast_qc(fq_path)
+                reqult_queq.put(out_dict)
+            except Exception as e:
+                reqult_queq.put({'error': sample_name})
+                raise ChildProcessError(e)
+        return
+
+    #  Creating queues for storing jobs and results
+    start = timeit.default_timer()
+    job_queq = multiprocessing.Queue()
+    reqult_queq = multiprocessing.Queue()
+    nproc = []
+
+    #  Counting number of CPUs and taking -1 for multiple process
+    no_cpus = int(multiprocessing.cpu_count() - 1)
+    print('Creating %d consumers for multiprocessing' % no_cpus)
+
+    #  initializing worker
+    for ii in range(no_cpus):
+        p = multiprocessing.Process(target=worker, args=(ii, job_queq, reqult_queq))
+        nproc.append(p)
+        p.start()
+
+    #  putting job in worker
+    for file in fq_filepaths:
+        job_queq.put(file)
+
+    #  Adding poison pill for information about job done
+    for i in range(no_cpus):
+        job_queq.put(None)
+
+    #  Retrieving job from result queue
+    result_dict = {}
+    #res_size = reqult_queq.qsize()
+    res_size = len(fq_filepaths)
+    print('Result queue size:', res_size)
+    while res_size:
+        result_dict.update(reqult_queq.get())
+        res_size -= 1
+
+    #  Joining all workers together and closing job queue (important for not creating Jombie process)
+    for p in nproc:
+        p.join()
+
+    stop = timeit.default_timer()
+    print(result_dict)
+    print('Time consumed in analysis:', stop-start, 'sec')
