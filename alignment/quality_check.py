a15f5e8d0a5d5fe93fc629b5f3aae94e26ca50df
peeyush <peeyushsahu@yahoo.com>
2017-07-16 02:42:07 +0200

    Multiprocessing in fastQC - Seq quality per Flowcell tile

--- alignment/quality_check.py
+++ alignment/quality_check.py
@@ -7,18 +7,20 @@ import sys
 import numpy as np
 import io
 import collections
+import multiprocessing
 from Bio.SeqUtils import GC
 
 path = '/ps/imt/Pipeline_development/raw_data/chipseq/singleEnd/40_K4me3_WT10_ChIP25_110915_TGACCA_L001_R1_003.fastq'
-zipped_path = '/ps/imt/Pipeline_development/raw_data/chipseq/singleEnd/40_K4me3_WT10_ChIP25_110915_TGACCA_L001_R1_003.fastq.gz'
-# zipped_path = '/home/peeyush/PycharmProjects/pipeline_development/part_test_file.fastq.gz'
+#zipped_path = '/ps/imt/Pipeline_development/raw_data/chipseq/singleEnd/40_K4me3_WT10_ChIP25_110915_TGACCA_L001_R1_003.fastq.gz'
+zipped_path = '/home/peeyush/PycharmProjects/pipeline_development/fastq_test_file.fastq.gz'
 
 base_freq_dict = {key: dict.fromkeys(['A', 'T', 'G', 'C', 'N'], 0) for key in range(51)}
 nucleotide_phred_quality_dict = {key: dict.fromkeys([i for i in range(1, 43, 1)], 0) for key in range(51)}
 seq_length_dist = dict.fromkeys(range(1, 101, 1), 0)
 seq_gc_dict = dict.fromkeys(range(0, 101, 1), 0)
 seq_qual_dict = dict.fromkeys(range(1, 43, 1), 0)
-flowcell_tile = set()
+flowcell_seq_count_dict = {}
+flowcell_tile_qual_dict = {}
 
 
 def fastq_iterator(file_obj):
@@ -46,10 +48,15 @@ def seq_qual_check(qual_str):
     seq_qual_dict[round(avg_qual)] += 1
 
 
-def seq_id_ops(id):
-    """Check for flowcell"""
-    id_elements = id.split(':')
-    flowcell_tile.add(id_elements[4])
+def seq_id_ops(seq_id, qual_str):
+    """Check for sequencing quality per tile"""
+    tile_id = seq_id.split(':')[4]
+    flowcell_seq_count_dict[tile_id] = flowcell_seq_count_dict.get(tile_id, 0) + 1
+    if tile_id not in flowcell_tile_qual_dict.keys():
+        flowcell_tile_qual_dict[tile_id] = dict.fromkeys(range(51), 0)
+    for ind, asci in enumerate(qual_str):
+        qual = ord(asci) - 33
+        flowcell_tile_qual_dict[tile_id][ind] += qual
 
 
 def nucleotide_operations(seq):
@@ -66,7 +73,7 @@ def nucleotide_operations(seq):
 
     # calculating % GC in seq
     letters = collections.Counter(seq)
-    GC = round(((letters['G'] + letters['C']) / 51) * 100)
+    GC = round(((letters['G'] + letters['C']) / 51.0) * 100)
     seq_gc_dict[GC] += 1
 
 
@@ -86,7 +93,7 @@ with gzip.open(zipped_path, "rb") as handle:
 
             nucleotide_operations(seq)
             seq_qual_check(phred_qual)
-            seq_id_ops(id)
+            seq_id_ops(id, phred_qual)
             i += 1
             # if i == 50000:
             #    break
@@ -128,12 +135,13 @@ with io.TextIOWrapper(gzip.open(zipped_path, "r")) as handle:
 print('Total seq analysed:', i)
 print('Time spent in creating the quality control:', (stop - start))
 
-print(seq_qual_dict)
-print(nucleotide_phred_quality_dict)
-print(base_freq_dict)
-print(seq_length_dist)
-print(seq_gc_dict)
-print(flowcell_tile)
+print('seq_qual_dict', seq_qual_dict)
+print('nucleotide_phred_quality_dict', nucleotide_phred_quality_dict)
+print('base_freq_dict', base_freq_dict)
+print('seq_length_dist', seq_length_dist)
+print('seq_gc_dict', seq_gc_dict)
+print('flowcell_tile_qual_dict', flowcell_tile_qual_dict)
+print('flowcell_seq_count_dict', flowcell_seq_count_dict)
 
 '''
 pd.DataFrame(nucleotide_phred_quality_dict, index=range(42, 0, -1)).to_csv('/ps/imt/Pipeline_development/results/AlignedLane/phred_quality_matrix.tsv', sep='\t')
@@ -164,7 +172,8 @@ def fast_qc(zipped_file_path):
     seq_length_dist = dict.fromkeys(range(1, 101, 1), 0)
     seq_gc_dict = dict.fromkeys(range(0, 101, 1), 0)
     seq_qual_dict = dict.fromkeys(range(1, 43, 1), 0)
-    flowcell_tile = set()
+    flowcell_tile_qual_dict = {}
+    flowcell_seq_count_dict = {}
 
     def fastq_iterator(file_obj):
         """ A very simple generater for reading fastq files"""
@@ -189,25 +198,28 @@ def fast_qc(zipped_file_path):
         avg_qual /= len(qual_str)
         seq_qual_dict[round(avg_qual)] += 1
 
-    def seq_id_ops(id):
-        """Check for flowcell"""
-        id_elements = id.split(':')
-        flowcell_tile.add(id_elements[4])
+    def flowcell_tile_seq_qual(seq_id, qual_str):
+        """Check for sequencing quality per tile"""
+        tile_id = seq_id.split(':')[4]
+        flowcell_seq_count_dict[tile_id] = flowcell_seq_count_dict.get(tile_id, 0) + 1
+        if tile_id not in flowcell_tile_qual_dict.keys():
+            flowcell_tile_qual_dict[tile_id] = dict.fromkeys(range(51), 0)
+        for ind, asci in enumerate(qual_str):
+            qual = ord(asci) - 33
+            flowcell_tile_qual_dict[tile_id][ind] += qual
 
     def nucleotide_operations(seq):
         """calculates frequency of nucleotide at every position"""
-        # calculating base freq
         max_length = 1000
         seq_len = len(seq)
         if seq_len > max_length:
-            raise ValueError('Not a valid fastq file OR sequence seq length > max_length')
-
+            raise ValueError('Not a valid fastq file OR sequence seq length > max_length' + str(max_length))
         seq_length_dist[len(seq)] += 1
         for ind, base in enumerate(seq):
             base_freq_dict[ind][base] += 1
         # calculating % GC in seq
         letters = collections.Counter(seq)
-        GC = round(((letters['G'] + letters['C']) / 51) * 100)
+        GC = round(((letters['G'] + letters['C']) / 51.0) * 100)
         seq_gc_dict[GC] += 1
 
     with gzip.open(zipped_file_path, "rb") as handle:
@@ -222,28 +234,90 @@ def fast_qc(zipped_file_path):
                     print(id)
                     print(seq)
                     print(phred_qual)
-                    raise ValueError('Broken fastq file...')
+                    raise ValueError('Broken fastq file, inspect the file...')
 
                 nucleotide_operations(seq)
                 seq_qual_check(phred_qual)
-                seq_id_ops(id)
+                flowcell_tile_seq_qual(id, phred_qual)
                 i += 1
         except Exception as e:
             print('number of seq analysed:' + str(i))
             raise ValueError(e)
         finally:
             handle.close()
+    print({'seq_qual_dict': seq_qual_dict,
+            'nucleotide_phred_quality_dict': nucleotide_phred_quality_dict,
+            'base_freq_dict': base_freq_dict,
+            'seq_length_dist': seq_length_dist,
+            'seq_gc_dict': seq_gc_dict,
+            'flowcell_tile_qual_dict': flowcell_tile_qual_dict,
+            'flowcell_seq_count_dict': flowcell_seq_count_dict
+            })
     return {'seq_qual_dict': seq_qual_dict,
             'nucleotide_phred_quality_dict': nucleotide_phred_quality_dict,
             'base_freq_dict': base_freq_dict,
             'seq_length_dist': seq_length_dist,
             'seq_gc_dict': seq_gc_dict,
-            'flowcell_tile': flowcell_tile
+            'flowcell_tile_qual_dict': flowcell_tile_qual_dict,
+            'flowcell_seq_count_dict': flowcell_seq_count_dict
             }
 
 
+def join_laneqc_result_dict(dict_result_dicts):
+    final_dict = {}
+    primary_keys = list(dict_result_dicts.keys())
+    print(primary_keys)
+    #  Joining single level dict
+    seq_qual_dict = dict_result_dicts[primary_keys[0]]['seq_qual_dict']
+    for key1 in seq_qual_dict.keys():
+        for keyn in primary_keys[1:]:
+            seq_qual_dict[key1] += dict_result_dicts[keyn]['seq_qual_dict'][key1]
+    final_dict['seq_qual_dict'] = seq_qual_dict
+
+    seq_length_dist = dict_result_dicts[primary_keys[0]]['seq_length_dist']
+    for key1 in seq_length_dist.keys():
+        for keyn in primary_keys[1:]:
+            seq_length_dist[key1] += dict_result_dicts[keyn]['seq_length_dist'][key1]
+    final_dict['seq_length_dist'] = seq_length_dist
+
+    seq_gc_dict = dict_result_dicts[primary_keys[0]]['seq_gc_dict']
+    for key1 in seq_gc_dict.keys():
+        for keyn in primary_keys[1:]:
+            seq_gc_dict[key1] += dict_result_dicts[keyn]['seq_gc_dict'][key1]
+    final_dict['seq_gc_dict'] = seq_gc_dict
+
+    flowcell_seq_count_dict = dict_result_dicts[primary_keys[0]]['flowcell_seq_count_dict']
+    for key1 in flowcell_seq_count_dict.keys():
+        for keyn in primary_keys[1:]:
+            flowcell_seq_count_dict[key1] += dict_result_dicts[keyn]['flowcell_seq_count_dict'][key1]
+    final_dict['flowcell_seq_count_dict'] = flowcell_seq_count_dict
+
+    #  joining two-level dict
+    nucleotide_phred_quality_dict = dict_result_dicts[primary_keys[0]]['nucleotide_phred_quality_dict']
+    for key1 in nucleotide_phred_quality_dict.keys():
+        for key11 in nucleotide_phred_quality_dict[key1].keys():
+            for keyn in primary_keys[1:]:
+                nucleotide_phred_quality_dict[key1][key11] += dict_result_dicts[keyn]['nucleotide_phred_quality_dict'][key1][key11]
+    final_dict['nucleotide_phred_quality_dict'] = nucleotide_phred_quality_dict
+
+    base_freq_dict = dict_result_dicts[primary_keys[0]]['base_freq_dict']
+    for key1 in base_freq_dict.keys():
+        for key11 in base_freq_dict[key1].keys():
+            for keyn in primary_keys[1:]:
+                base_freq_dict[key1][key11] += dict_result_dicts[keyn]['base_freq_dict'][key1][key11]
+    final_dict['base_freq_dict'] = base_freq_dict
+
+    flowcell_tile_qual_dict = dict_result_dicts[primary_keys[0]]['flowcell_tile_qual_dict']
+    for key1 in flowcell_tile_qual_dict.keys():
+        for key11 in flowcell_tile_qual_dict[key1].keys():
+            for keyn in primary_keys[1:]:
+                flowcell_tile_qual_dict[key1][key11] += dict_result_dicts[keyn]['flowcell_tile_qual_dict'][key1][key11]
+    final_dict['flowcell_tile_qual_dict'] = flowcell_tile_qual_dict
+
+    return final_dict
+
+
 def mp_fastqc(fq_filepaths):
-    import multiprocessing
 
     def worker(args, job_queq, reqult_queq):
 
@@ -303,4 +377,6 @@ def mp_fastqc(fq_filepaths):
 
     stop = timeit.default_timer()
     print(result_dict)
+    #print(join_laneqc_result_dict(result_dict))
     print('Time consumed in analysis:', stop-start, 'sec')
+    return join_laneqc_result_dict(result_dict)
